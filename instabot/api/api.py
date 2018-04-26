9048915e58e9729c9121ab3502bb7b44208529b5
Catalin <bardascat@gmail.com>
2018-04-26 16:46:33 +0300

    fix

--- instabot/api/api.py
+++ instabot/api/api.py
@@ -105,7 +105,7 @@ class API(object):
         blockLimit=12
         self.logger.info("canUserLogin: Checking if user with campaign: %s can loggin...", id_campaign)
         
-        result = fetchOne("select count(*) as total_blocks from instagram_log join campaign using(id_user) where date(instagram_log.timestamp)=CURDATE() and id_campaign=%s and (details='spam' or details='sentry_block')", id_campaign)
+        result = fetchOne("select count(*) as total_blocks from instagram_log join campaign using(id_user) where date(instagram_log.timestamp)=CURDATE() and id_campaign=%s and (details='spam' or details='sentry_block' or details='ip_block')", id_campaign)
         if result['total_blocks']>=blockLimit:
             self.logger.info("canUserLogin: Error: User cannot login because it was blocked more than %s times today. Actual block %s" % (blockLimit, result['total_blocks']))
             return False
@@ -145,7 +145,7 @@ class API(object):
             response=self.session.get('https://api.ipify.org?format=json')
             self.logger.info("Proxy test respone %s",response.text)
             
-            #todo: Probably it is not a good idea to keep the same cookie forever. The regular bot should recreate the cookie each morning !
+           
             if storage==False:
                 self.logger.info("login: Storage login is disabled !")
                 return self.newLogin(username,password,proxy)
@@ -202,7 +202,7 @@ class API(object):
         self.setUser(username, password)
         self.session.cookies.clear_session_cookies()
         
-        
+        self.logger.info("cookies 1: %s", self.session.cookies.list_domains())
         if (self.SendRequest('si/fetch_headers/?challenge_type=signup&guid=' + self.generateUUID(False), None, True)):
             
             data = {'phone_id': self.generateUUID(True),
@@ -212,9 +212,11 @@ class API(object):
             'device_id': self.device_id,
             'password': self.password,
             'login_attempt_count': '0'}
-
+            
+            self.logger.info("cookies 2: %s", self.session.cookies.list_domains())
+            
             if self.SendRequest('accounts/login/', self.generateSignature(json.dumps(data)), True):
-                
+                self.logger.info("cookies 3: %s", self.session.cookies.list_domains())    
                 userIdentity={}
                 self.isLoggedIn = True 
                     
@@ -229,19 +231,20 @@ class API(object):
                 insert("update campaign set instagram_username=%s where id_campaign=%s",self.LastJson['logged_in_user']['username'], self.id_campaign)
                 
                 
+                
                 userIdentity['uuid']=self.uuid
                 userIdentity['device_id']=self.device_id
                 userIdentity['user'] = self.LastJson["logged_in_user"]
                 userIdentity['token'] = self.LastResponse.cookies["csrftoken"]
-                userIdentity['csrftoken'] = self.session.cookies.get("csrftoken",domain=".instagram.com")
+                userIdentity['csrftoken'] = self.session.cookies.get("csrftoken",domain="i.instagram.com")
                 userIdentity['rank_token'] = self.rank_token
-                userIdentity['ds_user'] = self.session.cookies.get("ds_user",domain=".instagram.com")
-                userIdentity['ds_user_id'] = self.session.cookies.get("ds_user_id",domain=".instagram.com")
-                userIdentity['mid'] = self.session.cookies.get("mid",domain=".instagram.com")
-                userIdentity['rur'] = self.session.cookies.get("rur",domain=".instagram.com")
-                userIdentity['sessionid'] = self.session.cookies.get("sessionid",domain=".instagram.com")
-                userIdentity['social_hash_bucket_id'] = self.session.cookies.get("social_hash_bucket_id",domain=".instagram.com")
-                userIdentity['urlgen'] = self.session.cookies.get("urlgen",domain=".instagram.com")
+                userIdentity['ds_user'] = self.session.cookies.get("ds_user",domain="i.instagram.com")
+                userIdentity['ds_user_id'] = self.session.cookies.get("ds_user_id",domain="i.instagram.com")
+                userIdentity['mid'] = self.session.cookies.get("mid",domain="i.instagram.com")
+                userIdentity['rur'] = self.session.cookies.get("rur",domain="i.instagram.com")
+                userIdentity['sessionid'] = self.session.cookies.get("sessionid",domain="i.instagram.com")
+                userIdentity['social_hash_bucket_id'] = self.session.cookies.get("social_hash_bucket_id",domain="i.instagram.com")
+                userIdentity['urlgen'] = self.session.cookies.get("urlgen",domain="i.instagram.com")
                 
                 with io.open('/home/instabot-log/campaign/'+self.id_campaign+'/user-identity.json', 'w', encoding='utf-8') as f:
                     f.write(json.dumps(userIdentity, ensure_ascii=False))
@@ -350,6 +353,12 @@ class API(object):
                         self.logger.warning("sendRequest: ********** FATAL ERROR ************* sentry_block")
                         self.logApiError(responseInfo, currentOperation, config.API_URL, endpoint,response.status_code, details)
                         raise Exception("sendRequest: ********** FATAL ERROR ************* sentry_block")
+                        
+                    if responseObject['error_type'] == 'ip_block':
+                        details="ip_block"
+                        self.logger.warning("sendRequest: ********** FATAL ERROR ************* ip_block")
+                        self.logApiError(responseInfo, currentOperation, config.API_URL, endpoint,response.status_code, details)
+                        raise Exception("sendRequest: ********** FATAL ERROR ************* ip_block")
                     
                     self.logApiError(responseInfo, currentOperation, config.API_URL, endpoint,response.status_code, details)
                     
