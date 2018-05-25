25108817f55cdaba95144a723f3d9bcefbaace93
Catalin <bardascat@gmail.com>
2018-05-25 22:07:46 +0300

    fix

--- run/stop_bot.py
+++ run/stop_bot.py
@@ -24,8 +24,9 @@ def stopProcesses():
 	for p in psutil.process_iter():
 		cmdline=p.cmdline()
 		processname = 'angie_idc'
+		processnameInstapy = 'angie_instapy_idc' 
 		if len(cmdline)>0:
-			if processname in cmdline[0]:
+			if processname in cmdline[0] or processnameInstapy in cmdline[0]:
 				logger.info("stopProcesses:Found %s, pid %s, going to kill it" % (cmdline[0], p.pid))
 				killProcess(p.pid)
 				sleep_minutes = randint(1, 2)
