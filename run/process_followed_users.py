dff94b9b0650d647e741fc8e5879f313f3c635a6
Catalin <bardascat@gmail.com>
2019-07-09 18:06:28 +0300

    mongo stuff added

--- run/process_followed_users.py
+++ run/process_followed_users.py
@@ -1,8 +1,9 @@
+import datetime
+import glob
+import os
 import re
-import glob, os
-from instabot import Bot
-from instabot.api import api_db
 
+from instabot.api import api_db
 
 
 def getActiveUsers():
@@ -10,17 +11,17 @@ def getActiveUsers():
     users = api_db.select(query)
     return users
 
+
 def getFollowedUsers(path):
-    followedUsers=[]
-    files=[]
+    followedUsers = []
+    files = []
     os.chdir(path)
     for file in glob.glob("*.log"):
         files.append(file)
 
-
     for file in files:
-        filePath=path+"/"+file
-        print("Going to scan "+filePath)
+        filePath = path + "/" + file
+        print("Going to scan " + filePath)
         for i, line in enumerate(open(filePath)):
             m = re.search('(?<=Going to follow: )(.*)', line)
             if m is not None:
@@ -29,9 +30,66 @@ def getFollowedUsers(path):
 
     return followedUsers
 
-path="/home/instapy-log/campaign/logs"
+
+path = "/home/instapy-log/campaign/logs"
+#path = "/Users/cbardas/instapy-log/campaign/logs"
+users = getActiveUsers()
+
+
+def isUserFollowed(id_campaign, instagramUsername):
+    id_campaign = str(id_campaign)
+    print("checkIfUserIsFollowed: id_campaign:" + id_campaign + " instagramUsername:" + instagramUsername)
+    client = api_db.getMongoConnection()
+    db = client.angie_app
+    userFollowed = db.bot_action.find_one({"username": instagramUsername, "id_campaign": int(id_campaign),
+                                           "bot_operation": {"$regex": "^follow_engagement_"}})
+    client.close()
+    if userFollowed is None:
+        return False
+    print("User " + instagramUsername + " will be skipped, it's already in db!")
+    return True
+
+
+def markUserFollowed(id_campaign, instagramUsername, id_user):
+    client = api_db.getMongoConnection()
+    db = client.angie_app
+
+    db.bot_action.insert({
+        "id_campaign": id_campaign,
+        "id_user": id_user,
+        "instagram_id_user": None,
+        "full_name": instagramUsername,
+        "username": instagramUsername,
+        "user_image": None,
+        "post_id": None,
+        "post_image": None,
+        "post_link": None,
+        "bot_operation": "follow_engagement_by_hashtag",
+        "bot_operation_value": None,
+        "bot_operation_reverted": None,
+        "id_log": None,
+        "status": True,
+        "timestamp": datetime.datetime.now(),
+    })
+
+    client.close()
+    return True
+
+
 users = getActiveUsers()
 
 for user in users:
-    print("Going to process user"+ user["email"])
-    getFollowedUsers(path+"/"+user['id_campaign'])
+    print("Going to process angie user" + user["email"])
+    followerUsers = getFollowedUsers(path + "/" + str(user['id_campaign']))
+    print("Found " + str(len(users)) + " followed users for this user.")
+    print("Going to check with database")
+
+    for followedUser in followerUsers:
+        if isUserFollowed(user['id_campaign'], followedUser) == False:
+            print("User " + followedUser + " is not followed, going to save it in db.")
+            markUserFollowed(user['id_campaign'], followedUser, user['id_user'])
+    print("Done processing angie user" + user["email"])
+
+print("Done executing the script, going to exit !")
+
+# TODO: check if the users are in database, otherwise create the follow record.
